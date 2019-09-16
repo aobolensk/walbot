cf543cc122a7444aaf5f451ffec56c4b535160e1
Arseniy Obolenskiy <gooddoog@student.su>
2019-09-16 16:08:23 +0300

    (#5) Add function that gets all available commands in this scope

--- main.py
+++ main.py
@@ -4,8 +4,22 @@ import yaml
 
 
 class Commands:
-    def _get_member_list(self):
+    def __init__(self, config):
+        self.config = config
+
+    def _get_all_commands(self):
         return [func for func in dir(self) if callable(getattr(self, func)) and not func.startswith('_')]
+
+    def _get_available_commands(self, message):
+        result = []
+        for func in self._get_all_commands():
+            if (func in self.config.available_commands or
+                (message.channel.guild.id in self.config.guilds and
+                message.channel.id in self.config.guilds[message.channel.guild.id] and
+                func in self.config.guilds[message.channel.guild.id][message.channel.id]["available_commands"])):
+                result.append(func)
+        result.sort()
+        return result
 
     async def ping(self, message):
         """Check whether the bot is active"""
@@ -14,19 +28,23 @@ class Commands:
     async def help(self, message):
         """Print list of commands"""
         result = [method_name + ": " + getattr(self, method_name).__doc__
-                for method_name in self._get_member_list()
+                for method_name in self._get_available_commands(message)
                 if getattr(self, method_name).__doc__ is not None]
         await message.channel.send('\n'.join(result))
 
 
 class Config:
-
     def __init__(self):
-        self.commands = Commands()
+        self.commands = Commands(self)
         if not hasattr(self, "guilds"):
             self.guilds = dict()
         if not hasattr(self, "token"):
             self.token = None
+        if not hasattr(self, "available_commands"):
+            self.available_commands = {
+                "ping",
+                "help"
+            }
 
 
 class WalBot(discord.Client):
@@ -40,8 +58,11 @@ class WalBot(discord.Client):
             if guild.id not in self.config.guilds:
                 self.config.guilds[guild.id] = dict()
             for channel in guild.channels:
-                if isinstance(channel, discord.TextChannel):
-                    self.config.guilds[guild.id][channel.id] = None
+                if channel.id not in self.config.guilds[guild.id]:
+                    if isinstance(channel, discord.TextChannel):
+                        self.config.guilds[guild.id][channel.id] = {
+                            "available_commands": set()
+                        }
 
     async def on_message(self, message):
         if message.author.id == self.user.id:
@@ -50,8 +71,9 @@ class WalBot(discord.Client):
         if message.content.startswith('!'):
             command = message.content.split(' ')
             command[0] = command[0][1:]
-            if command[0] in self.config.commands._get_member_list():
-                command_name = self.config.commands._get_member_list()[self.config.commands._get_member_list().index(command[0])]
+            if command[0] in self.config.commands._get_available_commands(message):
+                command_name = (self.config.commands._get_available_commands(message)
+                    [self.config.commands._get_available_commands(message).index(command[0])])
                 await getattr(self.config.commands, command_name)(message)
             else:
                 await message.channel.send("Unknown command. !help for more information")
