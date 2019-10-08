ccbd2f0768b34cad20c09dcd92d2c194eb427573
Arseniy Obolenskiy <gooddoog@student.su>
2019-10-08 21:15:36 +0300

    (#20) Add reactions support for bot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import re
 import yaml
 
 
@@ -54,6 +55,10 @@ class Commands:
             self.data["whitelist"] = Command("whitelist",
                 perform=self._whitelist, permission=1)
             self.data["whitelist"].is_global = True
+        if "addreaction" not in self.data.keys():
+            self.data["addreaction"] = Command("addreaction",
+                perform=self._addreaction, permission=1)
+            self.data["addreaction"].is_global = True
         if "wme" not in self.data.keys():
             self.data["wme"] = Command("wme",
                 perform=self._wme, permission=1)
@@ -227,12 +232,27 @@ class Commands:
         else:
             await message.channel.send("Unknown argument '{}'".format(command[1]))
 
+    async def _addreaction(self, message, command):
+        """Add reaction
+        Example: !addreaction emoji regex"""
+        if len(command) < 3:
+            await message.channel.send("Too few arguments for command 'addreaction'")
+            return
+        self.config.reactions.append(Reaction(' '.join(command[2:]), command[1]))
+        await message.channel.send("Reaction '{}' on '{}' successfully added".format(command[1], ' '.join(command[2:])))
+
     async def _wme(self, message, command):
         """Send direct message to author with something"""
         if message.author.dm_channel is None:
             print("DMChannel is not created")
             await message.author.create_dm()
         await message.author.dm_channel.send(' '.join(command[1:]))
+
+
+class Reaction:
+    def __init__(self, regex, emoji):
+        self.regex = regex
+        self.emoji = emoji
 
 
 class GuildSettings:
@@ -252,6 +272,8 @@ class Config:
     def __init__(self):
         if not hasattr(self, "commands"):
             self.commands = Commands(self)
+        if not hasattr(self, "reactions"):
+            self.reactions = []
         self.commands.update_builtins()
         if not hasattr(self, "token"):
             self.token = None
@@ -280,6 +302,9 @@ class WalBot(discord.Client):
                 return
         if message.author.id not in self.config.users.keys():
             self.config.users[message.author.id] = User(message.author.id)
+        for reaction in self.config.reactions:
+            if re.search(reaction.regex, message.content):
+                await message.add_reaction(reaction.emoji)
         if message.content.startswith('!'):
             command = message.content.split(' ')
             command[0] = command[0][1:]
