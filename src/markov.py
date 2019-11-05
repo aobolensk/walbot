ed9da1f08eb31541307c16e569ba1708786c19a1
Arseniy Obolenskiy <gooddoog@student.su>
2019-11-05 15:44:10 +0300

    (#51) Add serialize() function for Markov model

--- src/markov.py
+++ src/markov.py
@@ -1,4 +1,27 @@
 import random
+import yaml
+
+
+class MarkovNode:
+    def __init__(self, markov, node_type, word=None):
+        self.markov = markov
+        self.type = node_type
+        if self.type == Markov.NodeType.word:
+            self.word = word
+        self.next = {None: 0}
+        self.total_next = 0
+
+    def add_next(self, word):
+        if word in self.next.keys():
+            self.next[word] += 1
+        else:
+            self.next[word] = 1
+
+    def get_next(self, word):
+        if word is not None:
+            return self.markov.model[word]
+        else:
+            return self.markov.end_node
 
 
 class Markov:
@@ -7,30 +30,9 @@ class Markov:
         word = 1
         end = 2
 
-    class Node:
-        def __init__(self, markov, node_type, word=None):
-            self.markov = markov
-            self.type = node_type
-            if self.type == Markov.NodeType.word:
-                self.word = word
-            self.next = {None: 0}
-            self.total_next = 0
-
-        def add_next(self, word):
-            if word in self.next.keys():
-                self.next[word] += 1
-            else:
-                self.next[word] = 1
-
-        def get_next(self, word):
-            if word is not None:
-                return self.markov.model[word]
-            else:
-                return self.markov.end_node
-
     def __init__(self):
-        self.model = {"": self.Node(self, self.NodeType.begin)}
-        self.end_node = self.Node(self, self.NodeType.end)
+        self.model = {"": MarkovNode(self, self.NodeType.begin)}
+        self.end_node = MarkovNode(self, self.NodeType.end)
 
     def add_string(self, text):
         words = text.split()
@@ -42,7 +44,7 @@ class Markov:
             if word in self.model.keys():
                 current_node = current_node.get_next(word)
             else:
-                current_node = self.model[word] = self.Node(
+                current_node = self.model[word] = MarkovNode(
                     self, self.NodeType.word, word=word
                 )
         current_node.add_next(None)
@@ -60,3 +62,7 @@ class Markov:
                     current_node = current_node.get_next(k)
                     break
         return result.strip()
+
+    def serialize(self, filename):
+        with open(filename, 'wb') as f:
+            f.write(yaml.dump(self, encoding='utf-8'))
