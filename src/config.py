b138816ff8a986a80b47d028eca43d545f181bb3
Arseniy Obolenskiy <gooddoog@student.su>
2019-11-23 21:35:42 +0300

    (#97) Fix linter remarks

--- src/config.py
+++ src/config.py
@@ -160,7 +160,7 @@ class Config:
             except IOError as e:
                 log.error("Unable to copy {} -> {}: {}".format(file, backup_file, e))
             else:
-               log.info("Created backup for {}: {}".format(file, backup_file))
+                log.info("Created backup for {}: {}".format(file, backup_file))
 
     def save(self, config_file, markov_file, wait=False):
         config_mutex = threading.Lock()
