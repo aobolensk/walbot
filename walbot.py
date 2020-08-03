67ddd195277f900dad63d6ca9fc130fb4d783360
Arseniy Obolenskiy <gooddoog@student.su>
2020-08-03 22:00:18 +0300

    Add missing flags for different walbot.py options

--- walbot.py
+++ walbot.py
@@ -14,15 +14,16 @@ class Launcher:
             (cmd, subparsers.add_parser(cmd, help=getattr(self, cmd).__doc__))
             for cmd in list(filter(lambda _: not _.startswith('_'), dir(self)))
         ])
-        # Start
-        sp["start"].add_argument("--fast_start", action="store_true",
-                                 help="Disable some things to make bot start faster:\n" +
-                                      "- Disable Markov model check on start\n")
-        sp["start"].add_argument("--patch", action="store_true",
-                                 help="Call script for patching config files before starting the bot")
-        if sys.platform in ("linux", "darwin"):
-            sp["start"].add_argument("--nohup", action="store_true",
-                                     help="Ignore SIGHUP and redirect output to nohup.out")
+        # Start & suspend
+        for x in ("start", "restart", "suspend"):
+            sp[x].add_argument("--fast_start", action="store_true",
+                               help="Disable some things to make bot start faster:\n" +
+                                    "- Disable Markov model check on start\n")
+            sp[x].add_argument("--patch", action="store_true",
+                               help="Call script for patching config files before starting the bot")
+            if sys.platform in ("linux", "darwin"):
+                sp[x].add_argument("--nohup", action="store_true",
+                                   help="Ignore SIGHUP and redirect output to nohup.out")
         # Docs
         sp["docs"].add_argument("-o", "--out_file", default=const.COMMANDS_DOC_PATH,
                                 help="Path to output file")
@@ -52,12 +53,12 @@ class Launcher:
         """Restart the bot"""
         bot = importlib.import_module("src.bot")
         bot.stop()
-        bot.start()
+        bot.start(self.args)
 
     def suspend(self):
         """Stop the main bot and start mini-bot"""
         self.stop()
-        importlib.import_module("src.bot").start(main_bot=False)
+        importlib.import_module("src.bot").start(self.args, main_bot=False)
 
     def docs(self):
         """Generate command docs"""
