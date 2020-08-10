078497cc6510a0d856be8ec37103d2bfda7ea1f7
Arseniy Obolenskiy <gooddoog@student.su>
2020-08-10 23:26:56 +0300

    Add more comments and constants in log.py

--- src/log.py
+++ src/log.py
@@ -13,9 +13,12 @@ class Log:
     DEBUG2: int = 9
     NOTSET: int = 0
 
+    # Logging constants
     MAX_FILESIZE: int = 3 * 1024 * 1024
+    LOGS_DIRECTORY: str = "logs"
 
     def debug2(self, msg, *args, **kwargs):
+        """Log with severity 'DEBUG2'."""
         self.log.log(self.DEBUG2, msg, *args, **kwargs)
 
     def __init__(self):
@@ -23,7 +26,9 @@ class Log:
             'version': 1,
             'disable_existing_loggers': True,
         })
+        # Add new logging levels
         logging.addLevelName(9, "DEBUG2")
+        # LOGGERS
         self.log = logging.getLogger("WalBot")
         self.log.setLevel(self.DEBUG2)
         # FORMATTERS
@@ -35,18 +40,18 @@ class Log:
         console_handler.setFormatter(formatter)
         self.log.addHandler(console_handler)
         # Create logs folder
-        if not os.path.exists("logs"):
-            os.makedirs("logs")
+        if not os.path.exists(self.LOGS_DIRECTORY):
+            os.makedirs(self.LOGS_DIRECTORY)
         # File handler (logs/error.log)
         err_log_file_hdl = logging.handlers.RotatingFileHandler(
-            os.path.join("logs", "error.log"), encoding="utf-8",
+            os.path.join(self.LOGS_DIRECTORY, "error.log"), encoding="utf-8",
             maxBytes=self.MAX_FILESIZE, backupCount=20)
         err_log_file_hdl.setLevel(self.ERROR)
         err_log_file_hdl.setFormatter(formatter)
         self.log.addHandler(err_log_file_hdl)
         # File handler (logs/walbot.log)
         general_log_file_hdl = logging.handlers.RotatingFileHandler(
-            os.path.join("logs", "walbot.log"), encoding="utf-8",
+            os.path.join(self.LOGS_DIRECTORY, "walbot.log"), encoding="utf-8",
             maxBytes=self.MAX_FILESIZE, backupCount=20)
         general_log_file_hdl.setLevel(self.DEBUG)
         general_log_file_hdl.setFormatter(formatter)
