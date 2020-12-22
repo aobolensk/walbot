8fbfb95612974c5df91725d010e45d08c186fe6e
Arseniy Obolenskiy <gooddoog@student.su>
2020-12-22 17:39:55 +0300

    Fix levenshtein_distance algorithm

--- src/algorithms.py
+++ src/algorithms.py
@@ -13,7 +13,7 @@ def levenshtein_distance(a: str, b: str):
         d[i, 0] = i
     for j in range(n + 1):
         d[0, j] = j
-    for j in range(n + 1):
-        for i in range(m + 1):
+    for j in range(1, n + 1):
+        for i in range(1, m + 1):
             d[i, j] = min(d[i-1, j] + 1, d[i, j-1] + 1, d[i-1, j-1] + int(a[i-1] != b[j-1]))
     return d[m, n]
