ca14997e321848fb24586edaf00f3fd88e56ffd2
Arseniy Obolenskiy <gooddoog@student.su>
2020-12-27 19:05:00 +0300

    Update massage about unsupported Python version

--- walbot.py
+++ walbot.py
@@ -10,8 +10,9 @@ import sys
 
 def main():
     """WalBot launcher entrypoint"""
-    if not (sys.version_info.major >= 3 and sys.version_info.minor >= 6):
-        print("Python {}.{}.{} is not supported. You need Python 3.6+".format(
+    if not ((sys.version_info.major == 3 and sys.version_info.minor >= 6) and
+            (sys.version_info.major == 3 and sys.version_info.minor <= 8)):
+        print("Python {}.{}.{} is not supported. You need Python 3.6 - 3.8".format(
             sys.version_info.major, sys.version_info.minor, sys.version_info.micro))
         sys.exit(1)
     importlib.import_module("src.launcher").Launcher()
