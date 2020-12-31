6f1304fa5cc027ece3d95fbb2443ec50fba503de
Arseniy Obolenskiy <gooddoog@student.su>
2020-12-31 18:28:20 +0300

    Use GitPython to retrieve commit SHA

--- src/info.py
+++ src/info.py
@@ -1,5 +1,6 @@
 import datetime
-import os
+
+import git
 
 from src.config import bc
 
@@ -7,20 +8,9 @@ from src.config import bc
 class BotInfo:
     @property
     def version(self):
-        if not os.path.exists(os.path.join(os.getcwd(), ".git")):
-            return "Unable to get version (.git folder is not found)"
-        if not os.path.exists(os.path.join(os.getcwd(), ".git/HEAD")):
-            return "Unable to get version (.git/HEAD file is not found)"
-        with open(os.path.join(os.getcwd(), ".git/HEAD")) as f:
-            branch = f.readline()
-            if branch[:5] != "ref: ":
-                return "Unable to get version (.git/HEAD format is unknown)"
-            branch = branch[5:].strip()
-        if not os.path.exists(os.path.join(os.getcwd(), ".git/" + branch)):
-            return "Unable to get version (.git/" + branch + " file is not found)"
-        with open(os.path.join(os.getcwd(), ".git/" + branch)) as f:
-            commit_hash = f.readline()
-        return commit_hash[:-1]
+        repo = git.Repo(search_parent_directories=True)
+        sha = repo.head.object.hexsha
+        return sha
 
     @property
     def uptime(self):
