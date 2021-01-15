7b1a186346b6934d4cc80ce23c3b7f22e170bc1b
Arseniy Obolenskiy <gooddoog@student.su>
2021-01-15 21:47:57 +0300

    Reorganize commands.py

--- src/commands.py
+++ src/commands.py
@@ -25,12 +25,13 @@ class Commands:
     def update(self):
         bc.commands = self
         cmd_directory = os.path.join(os.path.dirname(__file__), "cmd")
-        cmd_modules = ['.' + os.path.splitext(path)[0] for path in os.listdir(cmd_directory)
+        cmd_modules = ['src.cmd.' + os.path.splitext(path)[0] for path in os.listdir(cmd_directory)
                        if os.path.isfile(os.path.join(cmd_directory, path)) and path.endswith(".py")]
         for module in cmd_modules:
-            commands_file = importlib.import_module("src.cmd" + module)
+            log.debug2(f"Processing commands from module: {module}")
+            commands_file = importlib.import_module(module)
             commands = [obj[1] for obj in inspect.getmembers(commands_file, inspect.isclass)
-                        if (obj[1].__module__ == "src.cmd" + module) and issubclass(obj[1], BaseCmd)]
+                        if (obj[1].__module__ == module) and issubclass(obj[1], BaseCmd)]
             if len(commands) == 1:
                 commands = commands[0]
                 if "bind" in [func[0] for func in inspect.getmembers(commands, inspect.isfunction)
@@ -39,9 +40,9 @@ class Commands:
                 else:
                     log.error(f"Class '{commands.__name__}' does not have bind() function")
             elif len(commands) > 1:
-                log.error(f"Module 'src.cmd{module}' have more than 1 class in it")
+                log.error(f"Module '{module}' have more than 1 class in it")
             else:
-                log.error(f"Module 'src.cmd{module}' have no classes in it")
+                log.error(f"Module '{module}' have no classes in it")
         self.export_help(const.COMMANDS_DOC_PATH)
 
     def export_help(self, file_path):
@@ -52,27 +53,30 @@ class Commands:
             while repeat:
                 repeat = False
                 for name, command in self.data.items():
-                    if command.perform is not None:
-                        s = "**" + name + "**: "
-                        try:
-                            s += " \\\n".join(command.get_actor().__doc__.split('\n'))
-                        except AttributeError:
-                            del self.data[name]
-                            log.warning(f"Command '{name}' is not found and deleted from config and documentation")
-                            repeat = True
-                            break
-                        if command.subcommand:
-                            s += " \\\n    *This command can be used as subcommand*"
-                        s += '\n'
-                        s = s.replace('<', '&lt;').replace('>', '&gt;')
-                        result.append(s)
-            f.write('\n'.join(sorted(list(set(result)))))
+                    if command.perform is None:
+                        continue
+                    s = "**" + name + "**: "
+                    try:
+                        s += " \\\n".join(command.get_actor().__doc__.split('\n'))
+                    except AttributeError:
+                        del self.data[name]
+                        log.warning(f"Command '{name}' is not found and removed from config and documentation")
+                        repeat = True
+                        break
+                    if command.subcommand:
+                        s += " \\\n    *This command can be used as subcommand*"
+                    s += '\n'
+                    s = s.replace('<', '&lt;').replace('>', '&gt;')
+                    result.append(s)
+            f.write('\n'.join(sorted(result)))
 
     def register_command(self, module_name, class_name, command_name, **kwargs):
+        if command_name in self.data.keys():
+            log.debug2(f"Command {module_name} {class_name} {command_name} is already registered")
+            return
         log.debug2(f"Registering command: {module_name} {class_name} {command_name}")
-        if command_name not in self.data.keys():
-            if kwargs.get("message", None):
-                self.data[command_name] = Command(module_name, class_name, **kwargs)
-            else:
-                self.data[command_name] = Command(module_name, class_name, '_' + command_name, **kwargs)
-            self.data[command_name].is_global = True
+        if kwargs.get("message", None):
+            self.data[command_name] = Command(module_name, class_name, **kwargs)
+        else:
+            self.data[command_name] = Command(module_name, class_name, '_' + command_name, **kwargs)
+        self.data[command_name].is_global = True
