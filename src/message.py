27ea58c49308d563e1641aa2547b1c15c9ebf812
Arseniy Obolenskiy <gooddoog@student.su>
2021-01-16 14:18:36 +0300

    Introduce Msg.reply function

--- src/message.py
+++ src/message.py
@@ -9,6 +9,28 @@ class Msg:
             yield message[i:i+count]
 
     @staticmethod
+    async def reply(message, content, silent, **kwargs):
+        if not silent:
+            msg = None
+            if content:
+                for chunk in Msg.split_by_chunks(content, const.DISCORD_MAX_MESSAGE_LENGTH):
+                    msg = await message.reply(
+                        chunk,
+                        tts=kwargs.get("tts", False),
+                        files=kwargs.get("files", None))
+                    if kwargs.get("suppress_embeds", False):
+                        await msg.edit(suppress=True)
+            elif kwargs.get("files", None):
+                msg = await message.reply(None, files=kwargs.get("files", None))
+            if kwargs.get("embed", None):
+                msg = await message.reply(embed=kwargs["embed"], tts=kwargs.get("tts", False))
+                if kwargs.get("suppress_embeds", False):
+                    await msg.edit(suppress=True)
+            return msg
+        else:
+            log.info("[SILENT] -> " + content)
+
+    @staticmethod
     async def response(message, content, silent, **kwargs):
         if not silent:
             msg = None
