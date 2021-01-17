d80d6b431d4d88a12b4558fc831e3046d3013ea8
Arseniy Obolenskiy <gooddoog@student.su>
2021-01-17 22:01:26 +0300

    Propagate formatter to subparsers in argparse

--- src/launcher.py
+++ src/launcher.py
@@ -16,15 +16,17 @@ class Launcher:
         self._parser = argparse.ArgumentParser(description='WalBot', formatter_class=argparse.RawTextHelpFormatter)
         subparsers = self._parser.add_subparsers(dest="action")
         subparsers = {
-            cmd: subparsers.add_parser(cmd, help=getattr(self, cmd).__doc__)
+            cmd: subparsers.add_parser(
+                cmd, help=getattr(self, cmd).__doc__, formatter_class=argparse.RawTextHelpFormatter)
             for cmd in list(filter(lambda _: not _.startswith('_'), dir(self)))
         }
         # Start & suspend
         for option in ("start", "restart", "suspend"):
             subparsers[option].add_argument(
                 "--fast_start", action="store_true",
-                help="Disable some things to make bot start faster:\n" +
-                "- Disable Markov model check on start\n")
+                help=("Make bot start faster by disabling some optional checks:\n"
+                      "- Disable Markov model check on start\n"
+                      ))
             subparsers[option].add_argument(
                 "--patch", action="store_true",
                 help="Call script for patching config files before starting the bot")
