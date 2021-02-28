d51ca7479abf6bd7f002fc658867cb6a3dc5667d
Arseniy Obolenskiy <gooddoog@student.su>
2021-02-28 21:25:23 +0300

    (#372) Call patch if config versions were changed
    during 'autoupdate' subcommand execution

--- src/autoupdate.py
+++ src/autoupdate.py
@@ -1,6 +1,9 @@
+import importlib
 import os
-import time
 import sys
+import time
+from dataclasses import dataclass
+from typing import Optional
 
 import git
 
@@ -8,22 +11,55 @@ from src import const
 from src.log import log
 
 
-def check_updates(repo):
-    old_sha = repo.head.object.hexsha
+@dataclass
+class AutoUpdateContext:
+    config_version: Optional[str] = None
+    markov_version: Optional[str] = None
+    secret_version: Optional[str] = None
+    repo: Optional[git.Repo] = None
+
+    def __init__(self) -> None:
+        from src import const
+        self.config_version = const.CONFIG_VERSION
+        self.markov_version = const.MARKOV_CONFIG_VERSION
+        self.secret_version = const.SECRET_CONFIG_VERSION
+
+    def check_versions(self) -> bool:
+        from src import const
+        importlib.reload(const)
+        updated = False
+        if self.config_version != const.CONFIG_VERSION:
+            self.config_version = const.CONFIG_VERSION
+            updated = True
+        if self.markov_version != const.MARKOV_CONFIG_VERSION:
+            self.markov_version = const.MARKOV_CONFIG_VERSION
+            updated = True
+        if self.secret_version != const.SECRET_CONFIG_VERSION:
+            self.secret_version = const.SECRET_CONFIG_VERSION
+            updated = True
+        log.debug(f"Config versions were{'' if updated else ' not'} updated")
+        return updated
+
+
+def check_updates(context: AutoUpdateContext):
+    old_sha = context.repo.head.object.hexsha
     g = git.cmd.Git(os.getcwd())
     g.pull()
-    new_sha = repo.head.object.hexsha
+    new_sha = context.repo.head.object.hexsha
     log.debug(f"{old_sha} {new_sha}")
     if (old_sha == new_sha):
         return log.debug("No new updates")
     os.system(f"{sys.executable} -m pip install -r requirements.txt")
     os.system(f"{sys.executable} walbot.py stop")
+    if context.check_versions():
+        os.system(f"{sys.executable} walbot.py patch")
     os.system(f"{sys.executable} walbot.py start --nohup &")
 
 
 def start(args):
+    context = AutoUpdateContext()
     try:
-        repo = git.Repo(search_parent_directories=True)
+        context.repo = git.Repo(search_parent_directories=True)
     except git.exc.InvalidGitRepositoryError:
         return log.error("Failed to find walbot git repo. Autoupdate function is available only for git repository")
     if not os.path.isfile(const.BOT_CACHE_FILE_PATH):
@@ -31,7 +67,7 @@ def start(args):
         os.system(f"{sys.executable} walbot.py start --nohup &")
     try:
         while True:
-            check_updates(repo)
+            check_updates(context)
             time.sleep(const.AUTOUPDATE_CHECK_INTERVAL)
     except KeyboardInterrupt:
         os.system(f"{sys.executable} walbot.py stop")
