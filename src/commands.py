89e345dde5af34d6996432cb551c968f14a22627
Arseniy Obolenskiy <gooddoog@student.su>
2021-03-12 20:27:39 +0300

    (#382) Add support for recursive search in private cmds
    close #382

--- src/commands.py
+++ src/commands.py
@@ -22,14 +22,22 @@ class Commands:
         if not hasattr(self, "aliases"):
             self.aliases = dict()
 
+    def _path_to_module(self, path: str):
+        result = ''
+        for c in path:
+            if c != os.pathsep and c != '.' or result[-1] != '.':
+                result += c
+        return result
+
     def update(self, reload=False):
         bc.commands = self
         cmd_directory = os.path.join(os.path.dirname(__file__), "cmd")
         cmd_modules = ['src.cmd.' + os.path.splitext(path)[0] for path in os.listdir(cmd_directory)
                        if os.path.isfile(os.path.join(cmd_directory, path)) and path.endswith(".py")]
         private_cmd_directory = os.path.join(os.path.dirname(__file__), "cmd", "private")
-        cmd_modules += ['src.cmd.private.' + os.path.splitext(path)[0] for path in os.listdir(private_cmd_directory)
-                        if os.path.isfile(os.path.join(private_cmd_directory, path)) and path.endswith(".py")]
+        cmd_modules += [self._path_to_module(f"src.cmd.private.{os.path.relpath(path, private_cmd_directory)}.{os.path.splitext(file)[0]}")
+        for path, _, files in os.walk(private_cmd_directory) for file in files
+                        if os.path.isfile(os.path.join(private_cmd_directory, path, file)) and file.endswith(".py")]
         for module in cmd_modules:
             log.debug2(f"Processing commands from module: {module}")
             commands_file = importlib.import_module(module)
