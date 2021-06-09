72cdcd0d4a3a0ccf60414a81acc5450eb35e7b07
Arseniy Obolenskiy <gooddoog@student.su>
2021-06-09 16:40:49 +0300

    Send embeds in the same message with content in Msg.response function

--- src/message.py
+++ src/message.py
@@ -18,12 +18,13 @@ class Msg:
                 msg = await message.reply(
                     chunk,
                     tts=kwargs.get("tts", False),
-                    files=kwargs.get("files", None))
+                    files=kwargs.get("files", None),
+                    embed=kwargs.get("embed", None))
                 if kwargs.get("suppress_embeds", False):
                     await msg.edit(suppress=True)
         elif kwargs.get("files", None):
             msg = await message.reply(None, files=kwargs.get("files", None))
-        if kwargs.get("embed", None):
+        elif kwargs.get("embed", None):
             msg = await message.reply(embed=kwargs["embed"], tts=kwargs.get("tts", False))
             if kwargs.get("suppress_embeds", False):
                 await msg.edit(suppress=True)
@@ -39,12 +40,13 @@ class Msg:
                 msg = await message.channel.send(
                     chunk,
                     tts=kwargs.get("tts", False),
-                    files=kwargs.get("files", None))
+                    files=kwargs.get("files", None),
+                    embed=kwargs.get("embed", None))
                 if kwargs.get("suppress_embeds", False):
                     await msg.edit(suppress=True)
         elif kwargs.get("files", None):
             msg = await message.channel.send(None, files=kwargs.get("files", None))
-        if kwargs.get("embed", None):
+        elif kwargs.get("embed", None):
             msg = await message.channel.send(embed=kwargs["embed"], tts=kwargs.get("tts", False))
             if kwargs.get("suppress_embeds", False):
                 await msg.edit(suppress=True)
