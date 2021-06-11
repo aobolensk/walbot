a2e70b55a726d2d7b3c8d7ef67d1e3c62c1d615c
Arseniy Obolenskiy <gooddoog@student.su>
2021-06-12 00:25:54 +0300

    Remove user discriminator from 'added_by' field in existing quotes

--- src/patch/updater.py
+++ src/patch/updater.py
@@ -162,6 +162,10 @@ class Updater:
             config.commands.data["reminder"].subcommand = False
             self._bump_version(config, "0.0.26")
         if config.version == "0.0.26":
+            for index in config.quotes.keys():
+               config.quotes[index].added_by = config.quotes[index].added_by[:-5]
+            self._bump_version(config, "0.0.27")
+        if config.version == "0.0.27":
             log.info(f"Version of {self.config_path} is up to date!")
         else:
             log.error(f"Unknown version {config.version} for {self.config_path}!")
