73c543bf2c89299c18311050850b0d766b0ff864
Arseniy Obolenskiy <gooddoog@student.su>
2021-06-26 17:44:48 +0300

    Add test for removing from Markov model

--- tests/markov_tests.py
+++ tests/markov_tests.py
@@ -13,3 +13,10 @@ def test_markov_correctly_parses_sentence():
     text = LOREM.split()
     for i in range(len(text) - 1):
         assert text[i + 1] in markov.model[text[i]].next.keys()
+
+def test_markov_correctly_deletes_word():
+    markov = Markov()
+    markov.add_string(LOREM)
+    markov.del_words("ipsum")
+    assert "ipsum" not in markov.model["Lorem"].next.keys()
+    assert "ipsum" not in markov.model.keys()
