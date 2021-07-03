1f345964336c9653c48b334de59a5dda3e4b0bd9
Arseniy Obolenskiy <gooddoog@student.su>
2021-07-03 09:23:26 +0300

    Add type hints for MiniWalBot class methods

--- src/minibot.py
+++ src/minibot.py
@@ -1,25 +1,25 @@
 import discord
 
 from src import const
-from src.config import GuildSettings, User
+from src.config import Config, GuildSettings, SecretConfig, User
 from src.log import log
 
 
 class MiniWalBot(discord.Client):
-    def __init__(self, config, secret_config, bot_response):
+    def __init__(self, config: Config, secret_config: SecretConfig, bot_response: str) -> None:
         super().__init__()
         self.repl = None
         self.config = config
         self.secret_config = secret_config
         self.bot_response = bot_response
 
-    async def on_ready(self):
+    async def on_ready(self) -> None:
         log.info(f"Logged in as: {self.user.name} {self.user.id} ({self.__class__.__name__})")
         for guild in self.guilds:
             if guild.id not in self.config.guilds.keys():
                 self.config.guilds[guild.id] = GuildSettings(guild.id)
 
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message) -> None:
         try:
             if self.config.guilds[message.channel.guild.id].ignored:
                 return
