372885a0c36c8b179e7204c9f3c0dd29db437069
Arseniy Obolenskiy <gooddoog@student.su>
2021-07-24 00:10:52 +0300

    Add docs for FF class and move list inside the class

--- src/ff.py
+++ src/ff.py
@@ -1,21 +1,24 @@
 import os
 from typing import Dict
 
-_feature_flag_list = [
-    "WALBOT_FEATURE_NEW_CONFIG",
-]
 
 
 class FF:
     """Feature flag"""
 
+    _feature_flag_list = [
+        "WALBOT_FEATURE_NEW_CONFIG",
+    ]
+
     @staticmethod
     def is_enabled(feature_flag: str) -> bool:
-        if feature_flag not in _feature_flag_list:
+        """Get if feature flag is enabled"""
+        if feature_flag not in FF._feature_flag_list:
             raise ValueError(f"Incorrect feature flag: {feature_flag}")
         value = os.getenv(feature_flag)
         return value in ("1", "ON")
 
     @staticmethod
     def get_list() -> Dict[str, str]:
-        return {key: os.getenv(key) for key in _feature_flag_list}
+        """Get whole list of feature flags"""
+        return {key: os.getenv(key) for key in FF._feature_flag_list}
