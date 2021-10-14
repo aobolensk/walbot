158a378500ffb87fbee93eeaf0f74ed0bfe7d4ee
Arseniy Obolenskiy <gooddoog@student.su>
2021-10-15 01:36:47 +0300

    Refactor VoiceRoutine for better debugability

--- src/voice.py
+++ src/voice.py
@@ -21,51 +21,50 @@ class VoiceQueueEntry:
 class VoiceRoutine:
     def __init__(self, bot_cache) -> None:
         self.bot_cache = bot_cache
+        self._voice_client_queue_disconnect_counter = 0
 
     async def _update_autoupdate_flag(self, current_autoupdate_flag: bool) -> None:
         if current_autoupdate_flag != self.bot_cache.get_state()["do_not_update"]:
             self.bot_cache.update({"do_not_update": current_autoupdate_flag})
             self.bot_cache.dump_to_file()
 
-    @Mail.send_exception_info_to_admin_emails_async
     async def start(self) -> None:
         # Disconnect if bot is inactive in voice channel
-        voice_client_queue_disconnect_counter = 0
+
+        @Mail.send_exception_info_to_admin_emails_async
+        async def iteration():
+            if bc.voice_client is not None and not bc.voice_client_queue and not bc.voice_client.is_playing():
+                self._voice_client_queue_disconnect_counter += 1
+                if self._voice_client_queue_disconnect_counter >= 10:
+                    log.debug("Queue is empty. Disconnecting...")
+                    await bc.voice_client.disconnect()
+                    log.debug("Disconnected due to empty queue")
+                    bc.voice_client = None
+                    self._voice_client_queue_disconnect_counter = 0
+                    return
+            else:
+                self._voice_client_queue_disconnect_counter = 0
+            if bc.voice_client is None or not bc.voice_client_queue or bc.voice_client.is_playing():
+                return
+            if not bc.voice_client.is_connected():
+                log.debug("Connecting voice channel (1/2)...")
+                try:
+                    await bc.voice_client.connect()
+                except Exception as e:
+                    log.error(f"Failed to connect: {e}")
+                log.debug("Connecting voice channel (1/2)...")
+            if not bc.voice_client.is_playing():
+                entry = bc.voice_client_queue.popleft()
+                try:
+                    log.debug(f"Started to play {entry.file_name}")
+                    bc.voice_client.play(discord.FFmpegPCMAudio(entry.file_name))
+                except Exception as e:
+                    await entry.channel.send(f"ERROR: Failed to play: {e}")
+                await entry.channel.send(
+                    f"🔊 Now playing: {entry.title} (YT: {entry.id}) requested by {entry.requested_by}")
 
         while True:
             bc.do_not_update[DoNotUpdateFlag.VOICE] = bool(bc.voice_client)
             await self._update_autoupdate_flag(any(bc.do_not_update))
-            try:
-                if bc.voice_client is not None and not bc.voice_client_queue and not bc.voice_client.is_playing():
-                    voice_client_queue_disconnect_counter += 1
-                    if voice_client_queue_disconnect_counter >= 10:
-                        log.debug("Queue is empty. Disconnecting...")
-                        await bc.voice_client.disconnect()
-                        log.debug("Disconnected due to empty queue")
-                        bc.voice_client = None
-                        voice_client_queue_disconnect_counter = 0
-                        continue
-                else:
-                    voice_client_queue_disconnect_counter = 0
-                if bc.voice_client is None or not bc.voice_client_queue or bc.voice_client.is_playing():
-                    await asyncio.sleep(5)
-                    continue
-                if not bc.voice_client.is_connected():
-                    log.debug("Connecting voice channel (1/2)...")
-                    try:
-                        await bc.voice_client.connect()
-                    except Exception as e:
-                        log.error(f"Failed to connect: {e}")
-                    log.debug("Connecting voice channel (1/2)...")
-                if not bc.voice_client.is_playing():
-                    entry = bc.voice_client_queue.popleft()
-                    try:
-                        log.debug(f"Started to play {entry.file_name}")
-                        bc.voice_client.play(discord.FFmpegPCMAudio(entry.file_name))
-                    except Exception as e:
-                        await entry.channel.send(f"ERROR: Failed to play: {e}")
-                    await entry.channel.send(
-                        f"🔊 Now playing: {entry.title} (YT: {entry.id}) requested by {entry.requested_by}")
-            except Exception as e:
-                log.error(f"voice_routine logic failed: {e}")
+            await iteration()
             await asyncio.sleep(5)
