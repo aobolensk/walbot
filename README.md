b3b5b0be626c30bf1590c01eab13c75b4b26f71f
Arseniy Obolenskiy <gooddoog@student.su>
2021-12-21 12:34:56 +0300

    Increase minimal supported Python version to 3.8

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Discord bot in Python
 
 ### Requirements:
 - [ffmpeg](https://www.ffmpeg.org/)
-- [Python](https://www.python.org/) 3.7-3.10*
+- [Python](https://www.python.org/) 3.8-3.10*
 
 \* `numba` package is unavailable on Python 3.10, note that some functionality may be slower.
 
--- walbot.py
+++ walbot.py
@@ -11,9 +11,9 @@ import sys
 
 def main():
     """WalBot launcher entrypoint"""
-    if not ((sys.version_info.major == 3 and sys.version_info.minor >= 7) and
+    if not ((sys.version_info.major == 3 and sys.version_info.minor >= 8) and
             (sys.version_info.major == 3 and sys.version_info.minor <= 10)):
-        print("Python {}.{}.{} is not supported. You need Python 3.7 - 3.10".format(
+        print("Python {}.{}.{} is not supported. You need Python 3.8 - 3.10".format(
             sys.version_info.major, sys.version_info.minor, sys.version_info.micro))
         sys.exit(1)
     if os.path.normpath(os.getcwd()) != os.path.normpath(os.path.dirname(os.path.abspath(__file__))):
