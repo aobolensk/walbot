cff3b41ace858c9aee6fdd50ec29964f0224ccd6
Arseniy Obolenskiy <gooddoog@student.su>
2022-03-06 23:42:46 +0300

    Move 'listreminder' command to separate file on Telegram backend

--- src/backend/telegram/cmd/builtin.py
+++ src/backend/telegram/cmd/builtin.py
@@ -12,7 +12,6 @@ class BuiltinCommands:
     def add_handlers(self, dispatcher) -> None:
         dispatcher.add_handler(CommandHandler("ping", self._ping))
         dispatcher.add_handler(CommandHandler("markov", self._markov))
-        dispatcher.add_handler(CommandHandler("listreminder", self._listreminder))
         dispatcher.add_handler(CommandHandler("about", self._about))
 
     @Mail.send_exception_info_to_admin_emails
@@ -23,25 +22,6 @@ class BuiltinCommands:
     def _markov(self, update: Update, context: CallbackContext):
         result = bc.markov.generate()
         update.message.reply_text(result)
-
-    @Mail.send_exception_info_to_admin_emails
-    def _listreminder(self, update: Update, context: CallbackContext):
-        reminder_list = []
-        for index, reminder in bc.config.reminders.items():
-            rep = f' (repeats every {reminder.repeat_after} {reminder.repeat_interval_measure})'
-            prereminders = f' ({", ".join([str(x) + " min" for x in reminder.prereminders_list])} prereminders enabled)'
-            reminder_list.append(
-                (reminder.time,
-                 reminder.message,
-                 f"{index} at {reminder.time} "
-                 f" in <#{reminder.channel_id}> (Discord)'"
-                 f"{rep if reminder.repeat_after else ''}"
-                 f"{prereminders if reminder.prereminders_list else ''}"))
-        reminder_list.sort()
-        result = ""
-        for reminder in reminder_list:
-            result += f"{reminder[0]}: {reminder[1]} {reminder[2]}\n"
-        update.message.reply_text(result if result else "No reminders")
 
     @Mail.send_exception_info_to_admin_emails
     def _about(self, update: Update, context: CallbackContext):
---
+++ src/backend/telegram/cmd/reminder.py
@@ -0,0 +1,31 @@
+from src.config import bc
+from src.mail import Mail
+
+from telegram import Update
+from telegram.ext import CommandHandler, CallbackContext
+
+class ReminderCommands:
+    def __init__(self) -> None:
+        pass
+
+    def add_handlers(self, dispatcher) -> None:
+        dispatcher.add_handler(CommandHandler("listreminder", self._listreminder))
+
+    @Mail.send_exception_info_to_admin_emails
+    def _listreminder(self, update: Update, context: CallbackContext):
+        reminder_list = []
+        for index, reminder in bc.config.reminders.items():
+            rep = f' (repeats every {reminder.repeat_after} {reminder.repeat_interval_measure})'
+            prereminders = f' ({", ".join([str(x) + " min" for x in reminder.prereminders_list])} prereminders enabled)'
+            reminder_list.append(
+                (reminder.time,
+                 reminder.message,
+                 f"{index} at {reminder.time} "
+                 f" in <#{reminder.channel_id}> (Discord)'"
+                 f"{rep if reminder.repeat_after else ''}"
+                 f"{prereminders if reminder.prereminders_list else ''}"))
+        reminder_list.sort()
+        result = ""
+        for reminder in reminder_list:
+            result += f"{reminder[0]}: {reminder[1]} {reminder[2]}\n"
+        update.message.reply_text(result if result else "No reminders")
