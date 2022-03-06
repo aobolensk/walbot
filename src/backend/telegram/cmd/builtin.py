95ea491592dc262300ef760f390f8083f536fed7
Arseniy Obolenskiy <gooddoog@student.su>
2022-03-06 23:08:57 +0300

    Add mail sending decorators for Telegram commands

--- src/backend/telegram/cmd/builtin.py
+++ src/backend/telegram/cmd/builtin.py
@@ -1,4 +1,5 @@
 from src.config import bc
+from src.mail import Mail
 
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext
@@ -14,13 +15,16 @@ class BuiltinCommands:
         dispatcher.add_handler(CommandHandler("listreminder", self._listreminder))
         dispatcher.add_handler(CommandHandler("about", self._about))
 
+    @Mail.send_exception_info_to_admin_emails
     def _ping(self, update: Update, context: CallbackContext):
         update.message.reply_text('Pong!')
 
+    @Mail.send_exception_info_to_admin_emails
     def _markov(self, update: Update, context: CallbackContext):
         result = bc.markov.generate()
         update.message.reply_text(result)
 
+    @Mail.send_exception_info_to_admin_emails
     def _listreminder(self, update: Update, context: CallbackContext):
         reminder_list = []
         for index, reminder in bc.config.reminders.items():
@@ -39,6 +43,7 @@ class BuiltinCommands:
             result += f"{reminder[0]}: {reminder[1]} {reminder[2]}\n"
         update.message.reply_text(result if result else "No reminders")
 
+    @Mail.send_exception_info_to_admin_emails
     def _about(self, update: Update, context: CallbackContext):
         cmd_txt = context.args[0] if context.args else ""
         verbosity = 0
