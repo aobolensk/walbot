072c9316bf309833917ada6db2d30751112c058b
Arseniy Obolenskiy <gooddoog@student.su>
2022-04-17 17:40:49 +0300

    Add error mails on autoupdate

--- src/autoupdate_impl.py
+++ src/autoupdate_impl.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import sys
 import time
+import traceback
 from dataclasses import dataclass
 from typing import Optional
 
@@ -12,6 +13,8 @@ import psutil
 from src import const
 from src.bot_cache import BotCache
 from src.log import log
+from src.mail import Mail
+from src.utils import Util
 
 
 @dataclass
@@ -49,12 +52,36 @@ class AutoUpdateContext:
         return updated
 
 
+def get_autoupdate_error_message(error_string: str) -> str:
+    """Forms error message for autoupdate"""
+    return (
+        error_string +
+        "\n"
+        f"Backtrace:\n"
+        f"{''.join(traceback.format_stack())}\n"
+        f"Details:\n"
+        f"N/A\n"
+    )
+
+
 def check_updates(context: AutoUpdateContext) -> bool:
     """Function that performs updates check. It is called periodically"""
+    secret_config = Util.read_config_file(const.SECRET_CONFIG_PATH)
+    if secret_config is None:
+        return log.error("Failed to read secret config file")
+    mail = Mail(secret_config)
     old_sha = context.repo.head.object.hexsha
+    mail.send(
+        secret_config.admin_email_list,
+        "Autoupdate error",
+        get_autoupdate_error_message(f"Test notification\n"))
     try:
         context.repo.remotes.origin.fetch()
     except Exception as e:
+        mail.send(
+            secret_config.admin_email_list,
+            "Autoupdate error",
+            f"Failed to fetch updates from remote: {e}")
         return log.error(f"Fetch failed: {e}. Skipping this cycle, will try to update on the next one")
     new_sha = context.repo.remotes.origin.refs['master'].object.name_rev.split()[0]
     log.debug(f"{old_sha} {new_sha}")
@@ -75,13 +102,27 @@ def check_updates(context: AutoUpdateContext) -> bool:
         g.pull()
     except git.exc.GitCommandError as e:
         if "Connection timed out" in e.stderr or "Could not resolve host" in e.stderr:
+            mail.send(
+                secret_config.admin_email_list,
+                "Autoupdate error",
+                get_autoupdate_error_message(f"{e.command}: {e.stderr}"))
             log.warning(f"{e.command}: {e.stderr}")
         else:
             raise e
-    subprocess.call(f"{sys.executable} -m pip install -r requirements.txt", shell=True)
+    p = subprocess.run(f"{sys.executable} -m pip install -r requirements.txt", shell=True)
+    if p.returncode != 0:
+        mail.send(
+            secret_config.admin_email_list,
+            "Autoupdate error",
+            get_autoupdate_error_message(f"Failed to fetch requirements.txt"))
     minibot_response = "WalBot automatic update is in progress. Please, wait..."
     subprocess.call(f"{sys.executable} walbot.py startmini --message '{minibot_response}' --nohup &", shell=True)
-    subprocess.call(f"{sys.executable} walbot.py stop", shell=True)
+    p = subprocess.run(f"{sys.executable} walbot.py stop", shell=True)
+    if p.returncode != 0:
+        mail.send(
+            secret_config.admin_email_list,
+            "Autoupdate error",
+            get_autoupdate_error_message(f"Failed to stop the bot"))
     if context.check_versions():
         subprocess.call(f"{sys.executable} walbot.py patch", shell=True)
     subprocess.call(f"{sys.executable} walbot.py start --fast_start --nohup &", shell=True)
@@ -89,7 +130,12 @@ def check_updates(context: AutoUpdateContext) -> bool:
         time.sleep(1)
         bot_cache = importlib.import_module("src.bot_cache").BotCache(True).parse()
         if bot_cache is not None and bot_cache["ready"]:
-            subprocess.call(f"{sys.executable} walbot.py stopmini", shell=True)
+            p = subprocess.run(f"{sys.executable} walbot.py stopmini", shell=True)
+            if p.returncode != 0:
+                mail.send(
+                    secret_config.admin_email_list,
+                    "Autoupdate error",
+                    get_autoupdate_error_message(f"Failed to stop minibot"))
             log.info("Bot is fully loaded. MiniWalBot is stopped.")
             break
         log.debug("Bot is not fully loaded yet. Waiting...")
