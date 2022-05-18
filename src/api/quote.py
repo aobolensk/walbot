faec727a49a3b0b25cec37831d69fe36f360c233
Arseniy Obolenskiy <gooddoog@student.su>
2022-05-18 13:19:07 +0300

    Move Quote to api folder

---
+++ src/api/quote.py
@@ -0,0 +1,21 @@
+import datetime
+
+
+class Quote:
+    def __init__(self, message, added_by):
+        self.message = message
+        self.author = ""
+        self.added_by = added_by
+        self.timestamp = datetime.datetime.now().replace(microsecond=0)
+
+    def __str__(self):
+        return self.quote()
+
+    def get_author(self):
+        return ("(c) " + self.author) if self.author else ""
+
+    def quote(self):
+        return f"{self.message} {self.get_author()}"
+
+    def full_quote(self):
+        return f"{self.message} {self.get_author()}\nAdded by: {self.added_by[:-5]} at {self.timestamp}"
