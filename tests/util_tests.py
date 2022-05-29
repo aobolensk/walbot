00c0479e571b8467a5a53cc15dcb3fd986390e46
Arseniy Obolenskiy <gooddoog@student.su>
2022-05-29 13:43:13 +0000

    Extend cut_string tests

--- tests/util_tests.py
+++ tests/util_tests.py
@@ -9,3 +9,8 @@ def test_cut_string():
     assert Util.cut_string("a" * 5, 2) == "..."
     assert Util.cut_string("a" * 5, 1) == "..."
     assert Util.cut_string("a" * 5, 0) == "..."
+    assert Util.cut_string("abacaba", 100) == "abacaba"
+    assert Util.cut_string("abacaba", 8) == "abacaba"
+    assert Util.cut_string("abacaba", 7) == "abacaba"
+    assert Util.cut_string("abacaba", 6) == "aba..."
+    assert Util.cut_string("abacaba", 5) == "ab..."
