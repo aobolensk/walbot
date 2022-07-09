666e9ad9a06b3495f93366c6a69711993748531c
Arseniy Obolenskiy <gooddoog@student.su>
2022-07-09 21:10:01 +0300

    Add missing version info about googletrans dependency in 'about' command

--- src/info.py
+++ src/info.py
@@ -82,6 +82,7 @@ class BotInfo:
         res["PyYAML"] = importlib.import_module("yaml").__version__
         res["yt_dlp"] = importlib.import_module("yt_dlp.update").__version__
         res["python-telegram-bot"] = importlib.import_module("telegram").__version__
+        res["googletrans"] = importlib.import_module("googletrans").__version__
         return res
 
     @property
