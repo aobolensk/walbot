bf239ec7613ddd968b35a192c67649962830bf23
Arseniy Obolenskiy <gooddoog@student.su>
2022-07-18 14:59:53 +0300

    Fix formatting and update docstring

--- src/exception.py
+++ src/exception.py
@@ -2,7 +2,8 @@ import requests
 
 
 class HTTPRequestException(Exception):
-    """HTTP error"""
+    """HTTP request error"""
+
     def __init__(self, response: requests.Response):
         self.status_code = response.status_code
         super().__init__(f"HTTP request failed with status code: {response.status_code}")
