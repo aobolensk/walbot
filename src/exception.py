818ca8160f980a1ecf77fe14e4ebf68db04c99c1
Arseniy Obolenskiy <gooddoog@student.su>
2022-07-18 14:58:46 +0300

    Save status code in exception

--- src/exception.py
+++ src/exception.py
@@ -4,4 +4,5 @@ import requests
 class HTTPRequestException(Exception):
     """HTTP error"""
     def __init__(self, response: requests.Response):
+        self.status_code = response.status_code
         super().__init__(f"HTTP request failed with status code: {response.status_code}")
