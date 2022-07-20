06b56d9eaee39b6b2c2fae848ff11e40460a6d4f
Arseniy Obolenskiy <gooddoog@student.su>
2022-07-20 11:30:14 +0300

    Remove empty line

--- tests/markov_tests.py
+++ tests/markov_tests.py
@@ -1,6 +1,5 @@
 from src.markov import Markov
 
-
 LOREM = (
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
     "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
