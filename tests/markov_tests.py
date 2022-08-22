e5ff310f8969a724cd735453504d63db5773a0c3
Arseniy Obolenskiy <gooddoog@student.su>
2022-08-22 16:19:41 +0300

    Add more Markov tests

--- tests/markov_tests.py
+++ tests/markov_tests.py
@@ -20,3 +20,36 @@ def test_markov_correctly_deletes_word():
     markov.del_words("ipsum")
     assert "ipsum" not in markov.model["Lorem"].next.keys()
     assert "ipsum" not in markov.model.keys()
+
+
+def test_markov_correctly_deletes_word_with_regex():
+    markov = Markov()
+    markov.add_string(LOREM)
+    markov.del_words("^[a-z]{3}$")
+    assert "sit" not in markov.model["dolor"].next.keys()
+    assert "sed" not in markov.model["elit,"].next.keys()
+    assert "ipsum" in markov.model["Lorem"].next.keys()
+    assert "ut" in markov.model["incididunt"].next.keys()
+
+
+def test_markov_correctly_finds_word():
+    markov = Markov()
+    markov.add_string(LOREM)
+    found = markov.find_words("^[a-z]{3}$")
+    assert "sit" in found
+    assert "sed" in found
+    assert "ipsum" not in found
+    assert "ut" not in found
+
+
+def test_markov_check():
+    markov = Markov()
+    markov.add_string(LOREM)
+    assert markov.check() is True
+
+
+def test_markov_check_broken():
+    markov = Markov()
+    markov.add_string(LOREM)
+    markov.model[""].total_next = 0  # break next count for initial node
+    assert markov.check() is False
