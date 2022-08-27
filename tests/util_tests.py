b610bafe10a3bce5a32b6e0437d72dce3393a89e
Arseniy Obolenskiy <gooddoog@student.su>
2022-08-27 11:17:08 +0300

    Add basic test for Util.parse_int_for_command

--- tests/util_tests.py
+++ tests/util_tests.py
@@ -1,3 +1,6 @@
+import asyncio
+
+from src.test import BufferTestExecutionContext
 from src.utils import Util
 
 
@@ -14,3 +17,19 @@ def test_cut_string():
     assert Util.cut_string("abacaba", 7) == "abacaba"
     assert Util.cut_string("abacaba", 6) == "aba..."
     assert Util.cut_string("abacaba", 5) == "ab..."
+
+
+def test_parse_int_for_command(capsys):
+    loop = asyncio.new_event_loop()
+    value = loop.run_until_complete(
+        Util.parse_int_for_command(
+            BufferTestExecutionContext(), "123", "Error 1"))
+    assert value == 123
+    value = loop.run_until_complete(
+        Util.parse_int_for_command(
+            BufferTestExecutionContext(), "b123", "Error 2"))
+    assert value is None
+    captured = capsys.readouterr()
+    assert captured.out == (
+        "Error 2\n"
+    )
