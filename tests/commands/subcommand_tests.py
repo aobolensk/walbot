e475c1f44b924d403ed057c9cfa631304dfb1d8d
Arseniy Obolenskiy <gooddoog@student.su>
2022-08-30 00:12:36 +0300

    Fix autopep8 remarks

--- tests/commands/subcommand_tests.py
+++ tests/commands/subcommand_tests.py
@@ -22,11 +22,11 @@ def test_if_and_calc_commands_as_subcommands(capsys):
     bc.executor.add_module(MathCommands())
     loop = asyncio.new_event_loop()
     loop.run_until_complete(bc.executor.commands["echo"].run(
-            "echo $(if $(calc 2 < 3) less;not less)".split(" "), BufferTestExecutionContext()))
+        "echo $(if $(calc 2 < 3) less;not less)".split(" "), BufferTestExecutionContext()))
     loop.run_until_complete(bc.executor.commands["echo"].run(
-            "echo $(if $(calc 2 == 3) equal;not equal)".split(" "), BufferTestExecutionContext()))
+        "echo $(if $(calc 2 == 3) equal;not equal)".split(" "), BufferTestExecutionContext()))
     loop.run_until_complete(bc.executor.commands["echo"].run(
-            "echo $(if $(calc 2 > 3) greater;not greater)".split(" "), BufferTestExecutionContext()))
+        "echo $(if $(calc 2 > 3) greater;not greater)".split(" "), BufferTestExecutionContext()))
     captured = capsys.readouterr()
     assert captured.out == (
         "less\n"
