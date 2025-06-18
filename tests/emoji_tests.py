2e6fdfc92033b934508f633c422e92b79a922141
Arseniy Obolenskiy <gooddoog@student.su>
2025-06-18 07:52:56 +0200

    Add tests for get_clock_emoji

---
+++ tests/emoji_tests.py
@@ -0,0 +1,24 @@
+import pytest
+
+from src.emoji import get_clock_emoji
+
+
+@pytest.mark.parametrize("time_str, expected", [
+    ("00:00", "🕛"),
+    ("00:14", "🕛"),
+    ("00:15", "🕧"),
+    ("00:45", "🕐"),
+    ("02:37", "🕝"),
+    ("09:29", "🕤"),
+    ("12:44", "🕧"),
+    ("12:45", "🕐"),
+    ("13:01", "🕐"),
+    ("23:59", "🕛"),
+])
+def test_get_clock_emoji(time_str, expected):
+    assert get_clock_emoji(time_str) == expected
+
+
+@pytest.mark.parametrize("time_str", ["24:00", "12:60", "foo"])
+def test_get_clock_emoji_invalid(time_str):
+    assert get_clock_emoji(time_str) is None
