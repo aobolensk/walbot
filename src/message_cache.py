70e560822dda2c92608b0c4d88844383008e770b
Arseniy Obolenskiy <gooddoog@student.su>
2025-06-27 11:46:01 +0200

    Document MessageCache methods and add tests

--- src/message_cache.py
+++ src/message_cache.py
@@ -16,10 +16,29 @@ class MessageCache:
         self._data: Dict[str, List[CachedMsg]] = defaultdict(list)
 
     def push(self, channel_id: str, message: CachedMsg):
+        """Insert a new message to the beginning of the channel buffer.
+
+        Parameters
+        ----------
+        channel_id:
+            Identifier of the channel where the message was sent.
+        message:
+            ``CachedMsg`` instance representing the message to store.
+        """
         self._data[channel_id].insert(0, message)
         self._data[channel_id] = self._data[channel_id][:self.BUFFER_CAPACITY]
 
     def get(self, channel_id: str, index: int):
+        """Return cached message by index or ``None`` if unavailable.
+
+        Parameters
+        ----------
+        channel_id:
+            Identifier of the channel.
+        index:
+            Position of the message in the channel buffer. ``0`` is the most
+            recent message.
+        """
         if channel_id not in self._data.keys():
             return
         if not 0 <= index < len(self._data[channel_id]):
@@ -27,4 +46,13 @@ class MessageCache:
         return self._data[channel_id][index]
 
     def reset(self, channel_id: str, new_data: List[CachedMsg]):
+        """Replace stored messages for a channel.
+
+        Parameters
+        ----------
+        channel_id:
+            Identifier of the channel.
+        new_data:
+            List of ``CachedMsg`` objects that becomes the new buffer value.
+        """
         self._data[channel_id] = new_data
